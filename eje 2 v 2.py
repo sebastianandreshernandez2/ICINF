07fcbaa1261d925882ea8773fc0836c99f801f4a
sebastianandreshernandez2 <sebastianandres.hernandez2@alumnos.ulagos.cl>
2024-05-09 14:35:29 -0400

    Add files via upload

---
+++ eje 2 v 2.py
@@ -0,0 +1,11 @@
+dia=int
+hora=int 
+horas=int(input("horas trabajadas: "))
+valorhora=int(input("valor de la hora: "))
+
+mes=(hora*valorhora)
+dia=(valorhora*8)
+print("el sueldo diario es: ")
+print(dia)
+print("el sueldo mensual es: ")
+print(mes)
